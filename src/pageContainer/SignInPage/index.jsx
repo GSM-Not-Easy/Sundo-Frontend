9b6df4971f14dac6835192bff64d1205f9d05872
SeoJumee <official.jumee@gmail.com>
2023-12-20 10:15:34 +0900

    Add sign in page setting

---
+++ src/pageContainer/SignInPage/index.jsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const SignInPage = () => {
+  return <div>로그인페이지</div>;
+};
+
+export default SignInPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as SignUp } from './signup';
 export { default as ChatStart } from './chatStart';
+export { default as SignIn } from './signin';
