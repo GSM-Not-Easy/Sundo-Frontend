ec85fad10e03f26a2cb3baa0e36ced17d4e0130a
SeoJumee <official.jumee@gmail.com>
2023-12-20 10:38:57 +0900

    Add sign in page

--- src/pageContainer/SignInPage/index.jsx
+++ src/pageContainer/SignInPage/index.jsx
@@ -1,7 +1,46 @@
 import * as S from './style';
+import * as A from '../../assets/svg';
+import * as C from '../../components';
+import { Link } from 'react-router-dom';
 
 const SignInPage = () => {
-  return <div>로그인페이지</div>;
+  return (
+    <S.SignIn>
+      <S.SignInWrapper>
+        <S.SignInTitle>
+          <A.SignUpLogo width={170} height={64} />
+          <S.SignInText>GSM 선생님 통합 관리 서비스</S.SignInText>
+        </S.SignInTitle>
+        <S.SignInList>
+          <S.SignInItem>
+            <label>이메일</label>
+            <input type='email' placeholder='이메일을 입력해 주세요.' />
+          </S.SignInItem>
+          <S.SignInItem>
+            <label>비밀번호</label>
+            <input type='password' placeholder='비밀번호를 입력해 주세요.' />
+            <S.TextWrapper>
+              <S.PasswordToLink>비밀번호 찾기</S.PasswordToLink>
+            </S.TextWrapper>
+          </S.SignInItem>
+        </S.SignInList>
+        <C.Button
+          width='80%'
+          height='3rem'
+          color='#fff'
+          backgroundcolor='#6142F8'
+          borderradius='0.5rem'
+          margintop='1.625rem'
+        >
+          다음
+        </C.Button>
+        <S.LinkToSignUpText>
+          SUNDO가 처음이신가요?
+          <Link to='/signup'>회원가입</Link>
+        </S.LinkToSignUpText>
+      </S.SignInWrapper>
+    </S.SignIn>
+  );
 };
 
 export default SignInPage;
--- src/pageContainer/SignInPage/style.js
+++ src/pageContainer/SignInPage/style.js
@@ -1,1 +1,86 @@
 import styled from 'styled-components';
+
+export const SignIn = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f7f8fb;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignInWrapper = styled.div`
+  width: 24.125rem;
+  height: 29.375rem;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 0.5rem;
+  box-shadow: 0rem 0.25rem 0.5rem 0rem rgba(112, 144, 176, 0.12);
+`;
+
+export const SignInTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.25rem;
+  margin-top: 1.75rem;
+`;
+
+export const SignInText = styled.span`
+  color: #5d5d5d;
+`;
+
+export const SignInList = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.625rem;
+  margin-top: 1.5rem;
+`;
+
+export const SignInItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+
+  label {
+    font-size: 0.75rem;
+    color: #212121;
+  }
+
+  input {
+    padding: 1rem 0.75rem;
+    border: 0.0625rem solid #cacaca;
+    border-radius: 0.5rem;
+
+    &:focus {
+      border: 0.0625rem solid #6142f8;
+    }
+  }
+`;
+
+export const TextWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 0.25rem;
+`;
+
+export const PasswordToLink = styled.span`
+  font-size: 12px;
+  color: #888;
+`;
+
+export const LinkToSignUpText = styled.span`
+  padding-top: 0.75rem;
+  color: #888888;
+  font-size: 0.75rem;
+  margin-bottom: 1.5rem;
+
+  a {
+    color: #6142f8;
+    margin-left: 0.25rem;
+  }
+`;
