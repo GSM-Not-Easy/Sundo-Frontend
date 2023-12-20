18a2488bb5d6e9e537d45a4d38f59233b38a98e7
TaerangLee <s22051@gsm.hs.kr>
2023-12-20 20:27:51 +0900

    Delete the same router main

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,6 @@ export default function Router() {
         <Route path='/weeclass' element={<P.ChatStart />} />
         <Route path='/my' element={<P.My />} />
         <Route path='/stuinfo' element={<P.StuInfo />} />
-        <Route path='/' element={<P.Main />} />
         <Route path='/weestuchat' element={<P.WeeStuChatPage />} />
         <Route path='/weeteachat' element={<P.WeeTeaChatPage />} />
       </Routes>
