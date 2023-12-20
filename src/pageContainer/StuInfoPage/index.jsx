b19fc5b9b13fc631fa840d5870dc7e10732e3964
SeoJumee <official.jumee@gmail.com>
2023-12-20 13:20:47 +0900

    Add StuInfo page student list

--- src/pageContainer/StuInfoPage/index.jsx
+++ src/pageContainer/StuInfoPage/index.jsx
@@ -1,7 +1,79 @@
 import * as S from './style';
+import * as C from '../../components';
+import * as A from '../../assets/svg';
+import Girl from '../../assets/png/Girl.png';
+import Boy from '../../assets/png/Boy.png';
+import { useNavigate } from 'react-router-dom';
 
 const StuInfoPage = () => {
-  return <div>학생정보페이지</div>;
+  const navigate = useNavigate();
+  const students = [
+    {
+      classNum: '2209',
+      name: '서주미',
+      major: 'Front-End Develop',
+    },
+    {
+      classNum: '2405',
+      name: '김하온',
+      major: 'UI/UX Designer',
+    },
+    {
+      classNum: '2114',
+      name: '임준성',
+      major: 'Game Developer',
+    },
+    {
+      classNum: '2116',
+      name: '정윤서',
+      major: 'iOS Developer',
+    },
+  ];
+
+  return (
+    <S.StuInfo>
+      <C.Header />
+      <S.Content>
+        <S.TitleContainer>
+          <S.TitleWrapper>
+            <S.Title>학생정보</S.Title>
+            <S.SubTitle>{students.length}명</S.SubTitle>
+          </S.TitleWrapper>
+          <S.FilterButton>
+            <A.FilterIcon />
+            필터
+          </S.FilterButton>
+        </S.TitleContainer>
+        <S.StuList>
+          {students.map((student, idx) =>
+            idx % 2 === 0 ? (
+              <S.StuItem key={idx}>
+                <S.StuInfoSelect onClick={() => navigate(`/${student.name}`)}>
+                  눌러서 이동 <A.NavigateIcon />
+                </S.StuInfoSelect>
+                <img src={Girl} alt='여학생' />
+                <S.StuInfoContent>
+                  <S.Name>{`${student.classNum} ${student.name}`}</S.Name>
+                  <S.Major>{`${student.major}`}</S.Major>
+                </S.StuInfoContent>
+              </S.StuItem>
+            ) : (
+              <S.StuItem key={idx}>
+                <S.StuInfoSelect onClick={() => navigate(`/${student.name}`)}>
+                  눌러서 이동 <A.NavigateIcon />
+                </S.StuInfoSelect>
+                <img src={Boy} alt='남학생' />
+                <S.StuInfoContent>
+                  <S.Name>{`${student.classNum} ${student.name}`}</S.Name>
+                  <S.Major>{`${student.major}`}</S.Major>
+                </S.StuInfoContent>
+              </S.StuItem>
+            )
+          )}
+        </S.StuList>
+      </S.Content>
+    </S.StuInfo>
+  );
 };
 
 export default StuInfoPage;
--- src/pageContainer/StuInfoPage/style.js
+++ src/pageContainer/StuInfoPage/style.js
@@ -1,1 +1,144 @@
 import styled from 'styled-components';
+
+export const StuInfo = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  width: 65rem;
+  margin-top: 4.375rem;
+  flex-direction: column;
+`;
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: flex-end;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.75rem;
+  font-weight: 700;
+  color: #212121;
+`;
+
+export const SubTitle = styled.span`
+  font-size: 0.875rem;
+  font-weight: 400;
+  margin-bottom: 0.3125rem;
+  color: #888;
+`;
+
+export const FilterButton = styled.button`
+  width: 5rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border: 0.0625rem solid #6142f8;
+  color: #6142f8;
+  border-radius: 0.5rem;
+
+  &:hover {
+    background-color: #6142f8;
+    color: #fff;
+    path {
+      stroke: #fff;
+    }
+  }
+
+  &:active {
+    background-color: #3819d2;
+    color: #d7d1f6;
+    path {
+      stroke: #d7d1f6;
+    }
+  }
+`;
+
+export const StuList = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 0.75rem;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 1.625rem;
+`;
+
+export const StuItem = styled.div`
+  position: relative;
+  width: 15rem;
+  height: 17.5625rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0rem 0.25rem 0.75rem 0rem rgba(112, 144, 176, 0.12);
+  border: 0.0625rem solid #f5f5f5;
+
+  img {
+    width: 12.5rem;
+    height: 11.25rem;
+    margin-top: 1.5rem;
+  }
+`;
+
+export const StuInfoSelect = styled.div`
+  position: absolute;
+  width: 15rem;
+  height: 17.5625rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  color: #eee;
+  font-weight: 600;
+  gap: 0.25rem;
+
+  svg {
+    margin-bottom: 0.25rem;
+  }
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.4);
+    opacity: 1;
+  }
+`;
+
+export const StuInfoContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-left: 1.25rem;
+  margin-top: 0.875rem;
+  gap: 0.5rem;
+`;
+
+export const Name = styled.span`
+  font-size: 1.125rem;
+  font-weight: 600;
+  color: #212121;
+`;
+
+export const Major = styled.span`
+  color: #6142f8;
+  font-size: 0.75rem;
+  font-weight: 400;
+`;
