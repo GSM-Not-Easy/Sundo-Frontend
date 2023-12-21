bf01546fe458bec0708f98ec8ca4af9956155854
SeoJumee <official.jumee@gmail.com>
2023-12-21 19:55:30 +0900

    Add task list page

--- src/pageContainer/TaskPage/index.jsx
+++ src/pageContainer/TaskPage/index.jsx
@@ -1,7 +1,93 @@
 import * as S from './style';
+import * as C from '../../components';
+import { useEffect, useState } from 'react';
 
 const Task = () => {
-  return <div>과제 리스트</div>;
+  const tasks = [
+    {
+      num: '1',
+      title: '피그마프로토타이핑하기레전드스파이더맨티비오늘까지해오세용용용',
+      startDate: '2023.12.31',
+      endDate: '2024.01.01',
+      subject: 'HW',
+      grade: '3',
+    },
+    {
+      num: '2',
+      title: '네트워크와 데이터베이스 과목 보고서 과제 제출',
+      startDate: '2023.12.31',
+      endDate: '2024.01.08',
+      subject: 'SW',
+      grade: '2',
+    },
+  ];
+
+  const [selectedSubject, setSelectedSubject] = useState('');
+  const [selectedGrade, setSelectedGrade] = useState('');
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+
+  const handleSubjectChange = (e) => {
+    const subject = e.target.value;
+    setSelectedSubject(subject);
+  };
+
+  const handleGradeChange = (e) => {
+    const grade = e.target.value;
+    setSelectedGrade(grade);
+  };
+
+  useEffect(() => {
+    filterTasks(selectedSubject, selectedGrade);
+  }, [selectedSubject, selectedGrade]);
+
+  const filterTasks = (subject, grade) => {
+    if (subject === '' && grade === '') {
+      setFilteredTasks(tasks);
+    } else {
+      const filtered = tasks.filter((task) => {
+        return (
+          (subject === '' || task.subject === subject) &&
+          (grade === '' || task.grade === grade)
+        );
+      });
+      setFilteredTasks(filtered);
+    }
+  };
+
+  return (
+    <S.Task>
+      <C.Header />
+      <S.TaskWrapper>
+        <S.TitleWrapper>
+          <S.Title>과제</S.Title>
+          <S.ButtonWrapper>
+            <select value={selectedSubject} onChange={handleSubjectChange}>
+              <option value=''>과목</option>
+              <option value='SW'>SW</option>
+              <option value='HW'>HW</option>
+            </select>
+            <select value={selectedGrade} onChange={handleGradeChange}>
+              <option value=''>학년</option>
+              <option value='1'>1학년</option>
+              <option value='2'>2학년</option>
+              <option value='3'>3학년</option>
+            </select>
+          </S.ButtonWrapper>
+        </S.TitleWrapper>
+        <S.TaskList>
+          {filteredTasks.map((task, idx) => (
+            <S.TaskItem key={idx}>
+              <S.TaskNum>{task.num}</S.TaskNum>
+              <S.TaskTitle>{task.title}</S.TaskTitle>
+              <S.TaskDate>
+                {task.startDate} ~ {task.endDate}
+              </S.TaskDate>
+            </S.TaskItem>
+          ))}
+        </S.TaskList>
+      </S.TaskWrapper>
+    </S.Task>
+  );
 };
 
 export default Task;
--- src/pageContainer/TaskPage/style.js
+++ src/pageContainer/TaskPage/style.js
@@ -1,1 +1,84 @@
 import styled from 'styled-components';
+
+export const Task = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TaskWrapper = styled.div`
+  width: 65rem;
+  height: 100%;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2.8125rem;
+`;
+
+export const Title = styled.h1`
+  color: #212121;
+  font-weight: 700;
+  font-size: 1.75rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  select {
+    width: 4.5rem;
+    height: 2rem;
+    border-radius: 0.25rem;
+    border: 0.0625rem solid #6142f8;
+    font-size: 1rem;
+    font-weight: 400;
+    color: #6142f8;
+    outline: none;
+    padding-left: 0.75rem;
+  }
+`;
+
+export const TaskList = styled.div`
+  width: 100%;
+  margin-top: 1.25rem;
+`;
+
+export const TaskItem = styled.div`
+  width: 100%;
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 0.0625rem solid #eee;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fafafa;
+  }
+`;
+
+export const TaskNum = styled.span`
+  width: 33px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  color: #888;
+`;
+
+export const TaskTitle = styled.span`
+  width: 100%;
+  padding-left: 1.75rem;
+  color: #212121;
+`;
+
+export const TaskDate = styled.span`
+  white-space: nowrap;
+  padding-right: 2rem;
+`;
