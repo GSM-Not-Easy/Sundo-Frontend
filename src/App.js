eb3349280c70bbfd49c6e1cdb7e3ba2b7b4abaf4
TaerangLee <s22051@gsm.hs.kr>
2023-12-21 11:55:59 +0900

    Update ToastContainer import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { RecoilRoot } from 'recoil';
 import Router from './router';
 import GlobalStyles from './styles/GlobalStyles';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <RecoilRoot>
+      <ToastContainer />
+
       <GlobalStyles />
       <Router />
     </RecoilRoot>
