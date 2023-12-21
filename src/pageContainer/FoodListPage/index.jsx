e3009aced980e3cc5a188918b745a307d81b729b
SeoJumee <official.jumee@gmail.com>
2023-12-21 10:06:43 +0900

    Add food list page

--- src/pageContainer/FoodListPage/index.jsx
+++ src/pageContainer/FoodListPage/index.jsx
@@ -1,7 +1,71 @@
 import * as S from './style';
+import * as C from '../../components';
+import * as A from '../../assets/svg';
+import Girl from '../../assets/png/Girl.png';
+import Boy from '../../assets/png/Boy.png';
 
 const FoodListPage = () => {
-  return <div>급식 신청 리스트 페이지</div>;
+  const Food = [
+    {
+      classNum: '2222',
+      name: '김하온',
+      food: '마라탕',
+      text: '너무 먹고 싶어요',
+    },
+    {
+      classNum: '2222',
+      name: '서주미',
+      food: '감자탕',
+      text: '저는 감자탕을 사랑해요 감자탕 너무 마싯어 감자탕은 사랑입니다 ^^ 감자탕 존맛 해주세용 제발용 아잉뀨잉헿ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ',
+    },
+    {
+      classNum: '2222',
+      name: '서주미',
+      food: '감자탕',
+      text: '저는 감자탕을 사랑해요',
+    },
+    {
+      classNum: '2222',
+      name: '김하온',
+      food: '마라탕',
+      text: '너무 마쉿겟당',
+    },
+  ];
+  return (
+    <S.FoodList>
+      <C.Header />
+      <S.FoodWrapper>
+        <S.FoodTitle>신청급식</S.FoodTitle>
+        <S.FoodItemList>
+          {Food.map((Food, idx) =>
+            idx % 2 === 0 ? (
+              <S.FoodItem key={idx}>
+                <S.NameWrapper>
+                  <img src={Girl} alt='여학생' />
+                  <span>
+                    {Food.classNum} {Food.name}
+                  </span>
+                </S.NameWrapper>
+                <S.Food>{Food.food}</S.Food>
+                <S.Text>{Food.text}</S.Text>
+              </S.FoodItem>
+            ) : (
+              <S.FoodItem key={idx}>
+                <S.NameWrapper>
+                  <img src={Boy} alt='남학생' />
+                  <span>
+                    {Food.classNum} {Food.name}
+                  </span>
+                </S.NameWrapper>
+                <S.Food>{Food.food}</S.Food>
+                <S.Text>{Food.text}</S.Text>
+              </S.FoodItem>
+            )
+          )}
+        </S.FoodItemList>
+      </S.FoodWrapper>
+    </S.FoodList>
+  );
 };
 
 export default FoodListPage;
--- src/pageContainer/FoodListPage/style.js
+++ src/pageContainer/FoodListPage/style.js
@@ -1,1 +1,75 @@
 import styled from 'styled-components';
+
+export const FoodList = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const FoodWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 3.5rem;
+`;
+
+export const FoodTitle = styled.h1`
+  color: #212121;
+  font-size: 1.75rem;
+  font-weight: 700;
+`;
+
+export const FoodItemList = styled.div`
+  width: 65rem;
+  display: flex;
+  gap: 0.75rem;
+  flex-wrap: wrap;
+`;
+
+export const FoodItem = styled.div`
+  width: 21.125rem;
+  height: 7rem;
+  border: 0.0625rem solid #cacaca;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 1rem;
+  padding-bottom: 0;
+  border-radius: 0.5rem;
+`;
+
+export const NameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+
+  img {
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+  }
+
+  span {
+    color: #6142f8;
+    font-weight: 600;
+    font-size: 0.75rem;
+  }
+`;
+
+export const Food = styled.span`
+  color: #212121;
+  font-size: 1.125rem;
+  font-weight: 600;
+`;
+
+export const Text = styled.span`
+  color: #888;
+  font-size: 0.75rem;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
