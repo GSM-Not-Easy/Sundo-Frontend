0e7c8435d99be1ca53e5035390221b3ac9ee6cff
SeoJumee <official.jumee@gmail.com>
2023-12-21 11:37:31 +0900

    Update job style

--- src/pageContainer/JobPage/style.js
+++ src/pageContainer/JobPage/style.js
@@ -47,7 +47,7 @@ export const JobItem = styled.div`
 export const Info = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.25rem;
+  gap: 0.5rem;
 `;
 
 export const CompanyName = styled.span`
