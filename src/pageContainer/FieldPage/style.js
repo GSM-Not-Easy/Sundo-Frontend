573f237d34a443bc5dbdb68dd485c68a0eec5e41
SeoJumee <official.jumee@gmail.com>
2023-12-21 14:14:10 +0900

    Add field page

--- src/pageContainer/FieldPage/style.js
+++ src/pageContainer/FieldPage/style.js
@@ -1,1 +1,59 @@
 import styled from 'styled-components';
+
+export const Field = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FieldContainer = styled.div`
+  width: 65rem;
+  height: 100%;
+`;
+
+export const FieldContent = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 240px;
+    height: 240px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 3rem;
+`;
+
+export const Button = styled.button`
+  width: 8.75rem;
+  height: 2.75rem;
+  border-radius: 0.5rem;
+  border: 0.0625rem solid #6142f8;
+  color: #6142f8;
+  font-weight: 600;
+  font-size: 1rem;
+
+  &:hover {
+    color: #fff;
+    background-color: #6142f8;
+  }
+
+  &:active {
+    border: 0.0625rem solid #3819d2;
+    background-color: #3819d2;
+    color: #d7d1f6;
+  }
+`;
