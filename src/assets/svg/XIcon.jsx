f562de18eb8e2d27283a29e35531fc0dbdf14b3d
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-12-21 12:19:47 +0900

    Update XIcon.jsx

--- src/assets/svg/XIcon.jsx
+++ src/assets/svg/XIcon.jsx
@@ -10,9 +10,9 @@ const XIcon = () => {
       <path
         d='M4 12L12 4M4 4L12 12'
         stroke='#B2B2B2'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   );
