333ba96c4c7df97b12959584536c2cf6cb93884b
TaerangLee <s22051@gsm.hs.kr>
2023-12-21 09:26:00 +0900

    Update export AddIcon

---
+++ src/assets/svg/AddIcon.jsx
@@ -0,0 +1,21 @@
+const AddIcon = () => {
+  return (
+    <svg
+      width='1rem'
+      height='1rem'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M8 3V13M13 8H3'
+        stroke='#CACACA'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default AddIcon;
