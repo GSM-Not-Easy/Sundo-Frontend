65d1b2ea17d5cae7bd1a2ba294afb3b32815e1ac
TaerangLee <s22051@gsm.hs.kr>
2023-12-21 09:01:17 +0900

    Update export XIcon

---
+++ src/assets/svg/XIcon.jsx
@@ -0,0 +1,21 @@
+const XIcon = () => {
+  return (
+    <svg
+      width='1rem'
+      height='1rem'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M4 12L12 4M4 4L12 12'
+        stroke='#B2B2B2'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default XIcon;
