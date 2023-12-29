12a829cf7bcf645960dc0590bff1ba394dab09b8
TaerangLee <s22051@gsm.hs.kr>
2023-12-29 10:55:36 +0900

    Update queryKeys settings

--- src/lib/token.js
+++
@@ -1,4 +0,0 @@
-export const accessToken = 'SUNDO_accessToken';
-export const refreshToken = 'SUNDO_refreshToken';
-export const accessExp = 'SUNDO_accessExp';
-export const refreshExp = 'SUNDO_refreshExp';
