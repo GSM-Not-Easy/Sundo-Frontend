cae837a4d2d1f280d077333d427cfe43b1ad4652
TaerangLee <s22051@gsm.hs.kr>
2023-12-29 11:12:00 +0900

    token 고유 값을 추가합니다.

---
+++ src/api/src/libs/token.js
@@ -0,0 +1,4 @@
+export const accessToken = 'Sundo-accessToken';
+export const refreshToken = 'Sundo-refreshToken';
+export const accessExpiredAt = 'Sundo-accessExp';
+export const refreshExpiredAt = 'Sundo-refreshExp';
