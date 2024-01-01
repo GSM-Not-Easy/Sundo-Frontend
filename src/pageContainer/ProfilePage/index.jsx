2575470ce74316bfe5e27555bd5a66fb3f8cea1f
TaerangLee <s22051@gsm.hs.kr>
2024-01-02 08:20:59 +0900

    자기소개, 링크 첨부 퍼블리싱

---
+++ src/pageContainer/ProfilePage/index.jsx
@@ -0,0 +1,26 @@
+import * as S from './style';
+
+const ProfilePage = () => {
+  return (
+    <S.ProfileWrapper>
+      <S.ProfileContainer>
+        <S.IntroduceContainer>
+          <S.Title>자기소개</S.Title>
+          <S.ContentArea placeholder='과제에 대한 내용을 입력해 주세요.' />
+        </S.IntroduceContainer>
+        <S.LinkContainer>
+          <S.Title>소개링크</S.Title>
+          <S.Link placeholder='포트폴리오 링크를 기재해주세요' />
+          <S.Link placeholder='깃허브 링크를 기재해주세요' />
+          <S.Link placeholder='블로그 링크를 기재해주세요' />
+        </S.LinkContainer>
+        <S.MenuTitleContainer>
+          <S.MenuTitle>이전으로</S.MenuTitle>
+          <S.MenuTitle>확인</S.MenuTitle>
+        </S.MenuTitleContainer>
+      </S.ProfileContainer>
+    </S.ProfileWrapper>
+  );
+};
+
+export default ProfilePage;
