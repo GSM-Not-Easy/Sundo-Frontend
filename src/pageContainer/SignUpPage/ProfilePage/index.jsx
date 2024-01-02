df5656c00383b77c6af85ddc8c59d08afe86e0a4
SeoJumee <official.jumee@gmail.com>
2024-01-02 13:53:55 +0900

    Fix error

--- src/pageContainer/SignUpPage/ProfilePage/index.jsx
+++ src/pageContainer/SignUpPage/ProfilePage/index.jsx
@@ -9,13 +9,13 @@ const ProfilePage = () => {
       <S.ProfileContainer>
         <S.IntroduceContainer>
           <S.Title>자기소개</S.Title>
-          <S.ContentArea placeholder='과제에 대한 내용을 입력해 주세요.' />
+          <S.ContentArea placeholder='자기소개를 입력해 주세요.' />
         </S.IntroduceContainer>
         <S.LinkContainer>
           <S.Title>소개링크</S.Title>
-          <S.Link placeholder='포트폴리오 링크를 기재해주세요' />
           <S.Link placeholder='깃허브 링크를 기재해주세요' />
           <S.Link placeholder='블로그 링크를 기재해주세요' />
+          <S.Link placeholder='인스타그램 링크를 기재해주세요' />
         </S.LinkContainer>
         <S.MenuTitleContainer>
           <S.MenuTitle>이전으로</S.MenuTitle>
